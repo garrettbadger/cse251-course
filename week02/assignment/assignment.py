1fb2ca4fc277d0113bc584c4f775238be7d0d554
garrettbadger <garrett.badger.id@gmail.com>
2023-04-26 16:52:54 -0600

    week 2 assignment

--- week02/assignment/assignment.py
+++ week02/assignment/assignment.py
@@ -50,25 +50,132 @@ from cse251 import *
 TOP_API_URL = 'http://127.0.0.1:8790'
 
 # Global Variables
+global call_count
 call_count = 0
 
 
 # TODO Add your threaded class definition here
+class Request_thread(threading.Thread):
 
+    def __init__(self, url):
+        # Call the Thread class's init function
+        threading.Thread.__init__(self)
+        self.url = url
+        self.response = {}
+
+    def run(self):
+        response = requests.get(self.url)
+        # Check the status code to see if the request succeeded.
+        if response.status_code == 200:
+            self.response = response.json()
+        else:
+            print('RESPONSE = ', response.status_code)
 
 # TODO Add any functions you need here
+def characters(film6, threads_c, call_count):
 
+    for i in film6['characters']:
+      thread = Request_thread(i)
+      
+      threads_c.append(thread)
+    for i in threads_c:
+        i.start()
+        call_count += 1
+    for i in threads_c:
+        i.join()
+    for i in threads_c:
+        print(i.response['name'], end=", ")
+    print()
+    return call_count
 
+def planets(film6, threads_p, call_count):
+    for i in film6['planets']:
+        thread = Request_thread(i)
+        threads_p.append(thread)
+    for i in threads_p:
+        i.start()
+        call_count += 1
+    for i in threads_p:
+        i.join()
+    for i in threads_p:
+        print(i.response['name'], end=", ")
+    print()
+    return call_count
+
+def starships(film6, threads_s, call_count):
+    for i in film6['starships']:
+        thread = Request_thread(i)
+        threads_s.append(thread)
+    for i in threads_s:
+        i.start()
+        call_count += 1
+    for i in threads_s:
+        i.join()
+    for i in threads_s:
+        print(i.response['name'], end=", ")
+    print()
+    return call_count
+
+def vehicles(film6, threads_v, call_count):
+    for i in film6['vehicles']:
+        thread = Request_thread(i)
+        threads_v.append(thread)
+    for i in threads_v:
+        i.start()
+        call_count += 1
+    for i in threads_v:
+        i.join()
+    for i in threads_v:
+        print(i.response['name'], end=", ")
+    print()
+    return call_count
+
+def species(film6, threads_sp, call_count):
+    for i in film6['species']:
+        thread = Request_thread(i)
+        threads_sp.append(thread)
+    for i in threads_sp:
+        i.start()
+        call_count += 1
+    for i in threads_sp:
+        i.join()
+    for i in threads_sp:
+        print(i.response['name'], end=", ")
+    print()
+    return call_count
+
+# def display(threads):
+#     for i in threads:
+#       print(i)
+        
 def main():
     log = Log(show_terminal=True)
     log.start_timer('Starting to retrieve data from the server')
-
+    threads_c = []
+    threads_v = []
+    threads_s = []
+    threads_sp = []
+    threads_p = []
+    
     # TODO Retrieve Top API urls
-
+    top = Request_thread(TOP_API_URL)
+    top.start()
+    top.join()
+    
     # TODO Retireve Details on film 6
-
+    film6 = Request_thread(f'{top.response["films"]}6')
+    film6.start()
+    film6.join()
+    global call_count
+    call_count = characters(film6.response, threads_c, call_count)
+    call_count = planets(film6.response, threads_p, call_count)
+    call_count = starships(film6.response, threads_s, call_count)
+    call_count = vehicles(film6.response, threads_v, call_count)
+    call_count = species(film6.response, threads_sp, call_count)
+    
     # TODO Display results
-
+    # display(threads)
+    
     log.stop_timer('Total Time To complete')
     log.write(f'There were {call_count} calls to the server')
     
