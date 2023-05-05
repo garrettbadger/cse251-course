09c32a1610b9621fa054c7a53fed651805066f4a
garrettbadger <garrett.badger.id@gmail.com>
2023-05-05 09:45:30 -0600

    week3 assignment

--- week03/assignment/assignment.py
+++ week03/assignment/assignment.py
@@ -61,7 +61,14 @@ def create_new_frame(image_file, green_file, process_file):
 
 
 # TODO add any functions to need here
-
+def get_file_tuple():
+    tuple_list = []
+    for image_number in range(1, 301):
+        image_file = rf'elephant/image{image_number:03d}.png'
+        green_file = rf'green/image{image_number:03d}.png'
+        process_file = rf'processed/image{image_number:03d}.png'
+        tuple_list.append((image_file, green_file, process_file))
+    return tuple_list
 
 
 if __name__ == '__main__':
@@ -73,22 +80,25 @@ if __name__ == '__main__':
 
     xaxis_cpus = []
     yaxis_times = []
-
+    tuple_list = get_file_tuple()
     # TODO Process all frames trying 1 cpu, then 2, then 3, ... to CPU_COUNT
     #      add results to xaxis_cpus and yaxis_times
-
+    start_time = timeit.default_timer()
+    with mp.Pool(1) as p:
+        p.map(create_new_frame, tuple_list)
+    print(f'\nTime To Process all images = {timeit.default_timer() - start_time}')
 
     # sample code: remove before submitting  >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
     # process one frame #10
-    image_number = 10
+    # image_number = 10
 
-    image_file = rf'elephant/image{image_number:03d}.png'
-    green_file = rf'green/image{image_number:03d}.png'
-    process_file = rf'processed/image{image_number:03d}.png'
+    # image_file = rf'elephant/image{image_number:03d}.png'
+    # green_file = rf'green/image{image_number:03d}.png'
+    # process_file = rf'processed/image{image_number:03d}.png'
 
-    start_time = timeit.default_timer()
-    create_new_frame(image_file, green_file, process_file)
-    print(f'\nTime To Process all images = {timeit.default_timer() - start_time}')
+    # start_time = timeit.default_timer()
+    # create_new_frame(image_file, green_file, process_file)
+    # print(f'\nTime To Process all images = {timeit.default_timer() - start_time}')
     # <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
 
 
