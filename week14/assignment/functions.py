5458dfce71c968ef1fa6e1d59f681b3c198bf6c2
garrettbadger <garrett.badger.id@gmail.com>
2023-07-12 09:38:44 -0600

    week14

--- week14/assignment/functions.py
+++ week14/assignment/functions.py
@@ -1,6 +1,6 @@
 """
 Course: CSE 251, week 14
-File: common.py
+File: functions.py
 Author: <your name>
 
 Instructions:
@@ -13,14 +13,33 @@ https://www.youtube.com/watch?v=86g8jAQug04
 
 
 Requesting a family from the server:
-family = Request_thread(f'{TOP_API_URL}/family/{id}')
+request = Request_thread(f'{TOP_API_URL}/family/{id}')
+request.start()
+request.join()
+
+Example JSON returned from the server
+{
+    'id': 6128784944, 
+    'husband_id': 2367673859,        # use with the Person API
+    'wife_id': 2373686152,           # use with the Person API
+    'children': [2380738417, 2185423094, 2192483455]    # use with the Person API
+}
 
 Requesting an individual from the server:
-person = Request_thread(f'{TOP_API_URL}/person/{id}')
+request = Request_thread(f'{TOP_API_URL}/person/{id}')
+request.start()
+request.join()
 
+Example JSON returned from the server
+{
+    'id': 2373686152, 
+    'name': 'Stella', 
+    'birth': '9-3-1846', 
+    'parent_id': 5428641880,   # use with the Family API
+    'family_id': 6128784944    # use with the Family API
+}
 
-You will lose 10% if you don't detail your part 1 
-and part 2 code below
+You will lose 10% if you don't detail your part 1 and part 2 code below
 
 Describe how to speed up part 1
 
@@ -32,37 +51,93 @@ Describe how to speed up part 2
 <Add your comments here>
 
 
-10% Bonus to speed up part 3
+Extra (Optional) 10% Bonus to speed up part 3
 
 <Add your comments here>
 
 """
 from common import *
+import queue
 
 # -----------------------------------------------------------------------------
+# def depth_fs_pedigree(family_id, tree):
+#     # KEEP this function even if you don't implement it
+#     # TODO - implement Depth first retrieval
+#     # TODO - Printing out people and families that are retrieved from the server will help debugging
+#     # take the queue and empty it and make threads for the function to call persons and family
+#     # then join them and keep doing that until the queue doesn't have anything in it.
+    
+        
+#     # family = Request_thread(f'{TOP_API_URL}/family/{family_id}')
+#     # family.start()
+#     # family.join()
+#     # family_object = Family(family.get_response())
+#     # tree.add_family(family_object)
+#     # people_list = family.get_response()
+#     def dfs(family, id):
+#         if family:
+#             response = Request_thread(f'{TOP_API_URL}/family/{id}')
+#             response.start()
+#             response.join()
+#             family_object = Family(response.get_response())
+#             tree.add_family(family_object)
+#             for i in family_object.get_children():
+                
+#                 response = Request_thread(f'{TOP_API_URL}/person/{id}')
+#                 response.start()
+#                 response.join()
+#                 person_object = Person(response.get_response())
+#                 tree.add_person(person_object)
+#                 print(person_object)
+
+#     dfs_thread = threading.Thread(target=dfs, args=(True, family_id,))
+#     dfs_thread.start()
+#     dfs_thread.join()
+#     return tree
+#     pass
 def depth_fs_pedigree(family_id, tree):
-    # TODO - implement Depth first retrieval
+    
+    visited = set()
 
-    print('WARNING: DFS function not written')
-    # take the queue and empty it and make threads for the function to call persons and family
-    # then join them and keep doing that until the queue doesn't have anything in it.
+    def dfs(family_id):
+        if family_id in visited:
+            return
+        visited.add(family_id)
 
-    pass
+        # Request family information from the server
+        response = Request_thread(f'{TOP_API_URL}/family/{family_id}')
+        response.start()
+        response.join()
+        if response is not None:
+            print(response.get_response())
+            family = Family(response.get_response())
+            
+            tree.add_family(family)
 
+            # Recursively process the children families
+            children_families = family.get_children()
+            for child_family_id in children_families:
+                dfs(child_family_id)
+
+    # Perform multithreaded DFS
+    dfs_thread = threading.Thread(target=dfs, args=(family_id,))
+    dfs_thread.start()
+    dfs_thread.join()
+
+    return tree
 # -----------------------------------------------------------------------------
-def breadth_fs_pedigree(start_id, tree):
+def breadth_fs_pedigree(family_id, tree):
+    # KEEP this function even if you don't implement it
     # TODO - implement breadth first retrieval
-
-    print('WARNING: BFS function not written')
+    # TODO - Printing out people and families that are retrieved from the server will help debugging
 
     pass
 
-
 # -----------------------------------------------------------------------------
-def breadth_fs_pedigree_limit5(start_id, tree):
+def breadth_fs_pedigree_limit5(family_id, tree):
+    # KEEP this function even if you don't implement it
     # TODO - implement breadth first retrieval
     #      - Limit number of concurrent connections to the FS server to 5
-
-    print('WARNING: BFS (Limit of 5 threads) function not written')
+    # TODO - Printing out people and families that are retrieved from the server will help debugging
 
     pass
